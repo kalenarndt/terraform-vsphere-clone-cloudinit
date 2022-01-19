98dccd2f65dfcca71b180e7e5289c115657c268b
kalenarndt <kalenarndt@gmail.com>
2022-01-19 18:33:44 +0000

    fix settings.tf

--- settings.tf
+++ settings.tf
@@ -1,3 +1,9 @@
-provider "" {
-
+terraform {
+  experiments = [module_variable_optional_attrs]
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">=1.26.0"
+    }
+  }
 }
