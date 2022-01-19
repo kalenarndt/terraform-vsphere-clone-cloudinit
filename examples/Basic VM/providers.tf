68bf96252179032e2cc8fc56fb1051e71e19531b
kalenarndt <kalenarndt@gmail.com>
2022-01-19 18:10:00 +0000

    provider example

---
+++ examples/Basic VM/providers.tf
@@ -0,0 +1,6 @@
+provider "vsphere" {
+  # user                 = var.vcenter_username
+  # password             = var.vcenter_password
+  # vsphere_server       = var.vcenter_server
+  allow_unverified_ssl = true
+}
--- settings.tf
+++ settings.tf
@@ -1,9 +1,3 @@
-terraform {
-  experiments = [module_variable_optional_attrs]
-  required_providers {
-    vsphere = {
-      source  = "hashicorp/vsphere"
-      version = ">=1.26.0"
-    }
-  }
+provider "" {
+
 }
